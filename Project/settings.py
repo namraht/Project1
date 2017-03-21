0523fda8fa9df84d92aee55f8630113cfc081e36
namraht <namrah.tahir@ymail.com>
2017-03-21 23:57:43 +0500

    5th commit

--- Project/settings.py
+++ Project/settings.py
@@ -134,4 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
