39da450b20f7baf4a0a3730511390f462d66f47d
namraht <namrah.tahir@ymail.com>
2017-03-22 11:00:19 +0500

    =settings update

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=w!p951u9bwla4)wo5#qfev&2o0)&*ofy5&4rtu)z!y_i=x#g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
